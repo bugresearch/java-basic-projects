12a68a03c5bab726d33fc28e76628317be0baef1
Deniz Bektaş <cop@denizbektas.com.tr>
2021-05-04 19:30:46 +0300

    Create BasitDongu.java

---
+++ BasitDongu.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class BasitDongu {
+    public static void main(String args[]){
+        for(int i =0; i<10;i++){
+            System.out.println(i+1);
+        }
+    }
+}
