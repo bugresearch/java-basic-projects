3ab854a724dfee4ebac14dde3eccce95754cbeef
Deniz Bektaş <cop@denizbektas.com.tr>
2021-05-04 19:31:13 +0300

    Create DonguOrnegi.java

---
+++ DonguOrnegi.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class DonguOrnegi {
+    public static void main(String args[]){
+        for(int i = 0; i<7; i++){
+            System.out.println((7-i) * 13);
+        }
+    }
+}
